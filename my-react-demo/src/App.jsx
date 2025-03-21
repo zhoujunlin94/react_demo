c8efc1be21b697ed45453dc1c4befaf63ac54a03
zhoujunlin <zhoujunlin@gaodun.com>
2025-03-21 10:04:18 +0800

    0321

--- my-react-demo/src/App.jsx
+++ my-react-demo/src/App.jsx
@@ -1,8 +1,87 @@
-import { useRef, useState } from "react";
+import { useRef, useState, forwardRef } from "react";
+import { flushSync } from 'react-dom'
 
 
 
-export default function FocusInput() {
+
+
+export default function TodoList() {
+    const listRef = useRef(null);
+    const [text, setText] = useState('');
+    const [todos, setTodos] = useState(
+      initialTodos
+    );
+  
+    function handleAdd() {
+      const newTodo = { id: nextId++, text: text };
+      setText('');
+      //setTodos([ ...todos, newTodo]);
+      
+      // flushSync会让react立即更新DOM
+      flushSync(()=> {
+        setTodos([ ...todos, newTodo]);
+      })
+
+      // 更新state操作是进入队列  不会立即更新DOM 所以下面这个操作会滑动到lastChild-1的元素未知
+      listRef.current.lastChild.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest'
+      });
+    }
+  
+    return (
+      <>
+        <button onClick={handleAdd}>
+          添加
+        </button>
+        <input
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <ul ref={listRef}>
+          {todos.map(todo => (
+            <li key={todo.id}>{todo.text}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+  
+let nextId = 0;
+let initialTodos = [];
+for (let i = 0; i < 20; i++) {
+initialTodos.push({
+    id: nextId++,
+    text: '待办 #' + (i + 1)
+});
+}
+  
+
+
+
+
+const MyInput = forwardRef((props, ref)=>{
+    return <input {...props} ref={ref}/>
+});
+
+export function FocusInput2(){
+    const input = useRef(null)
+
+    function handleClick(){
+        input.current.focus()
+    }
+
+    return (
+        <>
+            <MyInput ref={input} />
+            <button onClick={handleClick}>聚焦输入框</button>
+        </>
+    )
+}
+
+
+
+export function FocusInput() {
     const input = useRef(null)
 
     function handleClick(){
